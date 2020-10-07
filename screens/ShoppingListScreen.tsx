05df3c82e6d9cd9c3e59e059c516e9ca651dff92
Mike Theodorou <c-michael.theodorou@charter.com>
2020-10-06 19:25:10 -0600

    adds draggable list

--- screens/ShoppingListScreen.tsx
+++ screens/ShoppingListScreen.tsx
@@ -1,19 +1,50 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
+const { useState } = React;
+import { TouchableOpacity, StyleSheet } from 'react-native';
 import { Text, View } from '../components/Themed';
+import DraggableFlatList from 'react-native-draggable-flatlist';
 
+const exampleData = [...Array(20)].map((d, index) => ({
+  key: `item-${index}`, // For example only -- don't use index as your key!
+  label: index,
+  backgroundColor: `rgb(${Math.floor(Math.random() * 255)}, ${
+    index * 5
+  }, ${132})`,
+}));
 export default function ShoppingListScreen() {
+  const [data, setData] = useState(exampleData);
+  const renderItem = ({ item, index, drag, isActive }) => {
+    return (
+      <TouchableOpacity
+        style={{
+          height: 100,
+          backgroundColor: isActive ? 'blue' : item.backgroundColor,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        onLongPress={drag}
+      >
+        <Text
+          style={{
+            fontWeight: 'bold',
+            color: 'white',
+            fontSize: 32,
+          }}
+        >
+          {item.label}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Shopping List</Text>
-      <View
-        style={styles.separator}
-        lightColor='#eee'
-        darkColor='rgba(255,255,255,0.1)'
+      <DraggableFlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => `draggable-item-${item.key}`}
+        onDragEnd={({ data }) => setData(data)}
       />
-      <EditScreenInfo path='/screens/ShoppingListScreen.js' />
     </View>
   );
 }
@@ -21,8 +52,8 @@ export default function ShoppingListScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
   title: {
     fontSize: 20,
